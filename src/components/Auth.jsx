ec09bbcb8c7d03ee1d2a112b7609e275f1883e7d
Faizan <sfaizan78692@gmail.com>
2024-03-23 08:29:18 +0530

    fix auth

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,27 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Login from "../pages/Login";
 import { useSelector } from "react-redux";
 
 function Auth() {
-  const [data, setData] = useState(null);
+  const { userData } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    let parsedUserData = null;
-    try {
-      const { userData } = useSelector((state) => state.user);
-      parsedUserData = JSON.parse(localStorage.getItem("userData")) || userData;
-    } catch (error) {
-      console.error("Error parsing userData from localStorage:", error);
-    }
-    setData(parsedUserData);
-  }, []);
-
-  if (!data) {
-    return null;
-  }
-
-  const token = data?.token || "";
+  const token =
+    userData?.token || JSON.parse(localStorage.getItem("userData")) || "";
   if (!token) {
     return <Login />;
   }
